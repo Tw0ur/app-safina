7c576bd72e9b826d36ad0e4adb2850e68b211c96
Madi <walparis1@mail.com>
2024-06-17 13:38:59 +0500

    fixed navbar content showing when hover over it

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Home, ChevronDown, LogOut, ArrowRightLeft } from "lucide-react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { ModeToggle } from "../buttons/modeToggle";
 
 const navItems = [
@@ -33,9 +33,23 @@ const navItems = [
 ];
 
 export const Nav = () => {
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const timeoutRef = useRef(null);
+
+  const handleMouseEnter = (index) => {
+    clearTimeout(timeoutRef.current);
+    setActiveIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setActiveIndex(-1);
+    }, 50); // Adjust the delay as needed
+  };
+
   return (
     <>
-      {/* // For larger screens and over */}
+      {/* For larger screens and over */}
       <nav className="hidden lg:flex justify-around items-center py-2 shadow-md dark:border dark:border-b-gray-600">
         <ul className="w-full flex flex-1 justify-around items-start">
           {navItems.map((item, ind) => {
@@ -43,7 +57,9 @@ export const Nav = () => {
             return (
               <li
                 key={ind}
-                className="relative flex flex-col items-center group"
+                className="relative flex flex-col items-center"
+                onMouseEnter={() => handleMouseEnter(ind)}
+                onMouseLeave={handleMouseLeave}
               >
                 <a
                   href="#"
@@ -53,13 +69,21 @@ export const Nav = () => {
                   <p>{item.name}</p>
                   {item.dropdownItem.length !== 0 && (
                     <ChevronDown
-                      className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-hover:rotate-180"
+                      className={`relative top-[1px] ml-1 h-3 w-3 transition duration-200 ${
+                        activeIndex === ind ? "rotate-180" : ""
+                      }`}
                       aria-hidden="true"
                     />
                   )}
                 </a>
                 {item.dropdownItem.length > 0 && (
-                  <ul className="absolute z-10 top-full mt-9 md:mt-4 flex flex-col border  shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+                  <ul
+                    className={`absolute z-10 top-full mt-2 flex flex-col border shadow-md transition duration-200 bg-white dark:bg-gray-800 ${
+                      activeIndex === ind ? "block" : "hidden"
+                    }`}
+                    onMouseEnter={() => handleMouseEnter(ind)}
+                    onMouseLeave={handleMouseLeave}
+                  >
                     {item.dropdownItem.map((dropdownItem, dropdownInd) => (
                       <li
                         key={dropdownInd}
@@ -84,7 +108,7 @@ export const Nav = () => {
           </a>
         </ul>
       </nav>
-      {/* For x < lg  */}
+      {/* For smaller screens */}
       <nav className="flex lg:hidden justify-between items-center px-2 py-2 shadow-md dark:border dark:border-b-gray-600"></nav>
     </>
   );
